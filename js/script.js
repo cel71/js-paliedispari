454f90bdedf6a9dd266482363a88127280d4fa0a
cel71 <celestialc71@libero.it>
2021-06-09 19:14:03 +0200

    js-paliedispari es2

--- js/script.js
+++ js/script.js
@@ -24,25 +24,34 @@ if(parolaUtente === parolaInversa){
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // // Infine, dichiariamo chi ha vinto.
 
-
 var pariDispari = prompt("Scegli pari o dispari");
 var numeriUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
-var random = Math.floor(Math.random() * 5) + 1;
-var somma = random + numeriUtente;
+
+function numeroCasuale() {
+    var random = Math.floor(Math.random() * 5) + 1;
+    return random;
+}
+
+var somma = numeroCasuale() + numeriUtente;
 console.log(somma);
+var x = "pari";
 
 function pariDis (n) {
-    if (n % 2 === 0 && pariDispari === "pari") {
+    if (n % 2 === 0) {
         return true;
     } else {
         return false;
     }
 }
 
-if (pariDis(pariDispari)) {
+var risultato = pariDis(somma);
+
+if (risultato === true && pariDispari === x) {
     document.getElementById("numeri").innerHTML = "Il numero è pari";
-} else {
+} else if (risultato === false && pariDispari !== x) {
     document.getElementById("numeri").innerHTML = "Il numero è dispari";
+} else {
+    document.getElementById("numeri").innerHTML = "La somma e la parola inserita sono opposti";
 }
 
 
